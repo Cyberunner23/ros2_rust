3907892c8665abe37bae5acb7645c5987b81ae42
Alex FL <alex@nexusrobotics.ca>
2023-05-07 00:20:47 +0000

    Finalized comments and removed unused import

--- rclrs/src/log/log_context.rs
+++ rclrs/src/log/log_context.rs
@@ -1,4 +1,3 @@
-use std::ptr::null;
 use std::sync::{Arc, Mutex};
 
 use lazy_static::lazy_static;
@@ -19,27 +18,27 @@ lazy_static! {
     static ref GLOBAL_LOG_CONTEXT: Arc<Mutex<Option<LogContext>>> = Arc::new(Mutex::new(None));
 }
 
-// 
+// Exisis only to uninitialize the logging system on exit.
 pub(crate) struct LogContext;
 
 impl Drop for LogContext {
     fn drop(&mut self) {
 
-        // THREAD SAFETY: Satisfies requirement to lock on uninitialize
+        // THREAD SAFETY: Satisfies requirement to lock on uninitialize.
         let global_context = GLOBAL_LOG_CONTEXT.clone();
         let mut global_context_guard = global_context.lock().unwrap();
 
         if global_context_guard.is_none() {
-            // Context already uninitialized
+            // Context already uninitialized.
             return;
         }
 
         unsafe {
-            // SAFETY: Fini is expected for an initialized logging system
+            // SAFETY: Fini is expected for an initialized logging system.
             let _unused = rcl_logging_fini().ok();
         }
 
-        // Marks rcl logging as uninitialized
+        // Marks rcl logging as uninitialized.
         *global_context_guard = None;
     }
 }
@@ -49,27 +48,42 @@ impl LogContext {
         Self {}
     }
 
-    pub(crate) fn init(rcl_context: &Context, allocator: &rcutils_allocator_t) -> Result<(), RclrsError> {
+    pub(crate) fn init(rcl_context: &Context) -> Result<(), RclrsError> {
 
-        // THREAD SAFETY: Satisfies requirement to lock on initialize
+        // THREAD SAFETY:
+        //   Satisfies requirement to lock on initialize.
+        //   There exists a dependency on rcl context,
+        //   however rcl context does not depend on our (rclrs) logging mutex.
+        //   Therefore, deadlocks are not possible.
         let global_context = GLOBAL_LOG_CONTEXT.clone();
         let mut global_context_guard = global_context.lock().unwrap();
 
         if global_context_guard.is_some() {
-            // Context already created
+            // Context already created.
             return Ok(());
         }
         
         {
+            // THREAD SAFETY:
+            //   This is our dependency on rcl context,
+            //   however rcl context does not depend on our (rclrs) logging mutex.
+            //   Therefore, deadlocks are not possible.
             let rcl_context_mtx = rcl_context.rcl_context_mtx.clone();
             let rcl_context_mtx_guard = rcl_context_mtx.lock().unwrap();
 
             unsafe {
-                // SAFETY: !! TODO: SAFETY ANALYSIS !!
+                // SAFETY: No preconditions for this function.
+                let allocator = rcutils_get_default_allocator();
+
+                // SAFETY:
+                //   It is expected to pass the global_arguments of a non-zero initialized rcl context.
+                //   It is expected to pass a non-zero initialized allocator.
+                //   It is expected to pass a callback for output handling.
                 rcl_logging_configure_with_output_handler(
                     &rcl_context_mtx_guard.global_arguments,
-                    allocator,
-                    Some(rclrc_logging_output_handler)).ok()?;
+                    &allocator,
+                    Some(rclrc_logging_output_handler)
+                ).ok()?;
             }
         }
 
@@ -87,13 +101,13 @@ unsafe extern "C" fn rclrc_logging_output_handler(
     format: *const ::std::os::raw::c_char,
     args: *mut va_list) {
 
-    // THREAD SAFETY: Satisfies requirement to lock on output handling
+    // THREAD SAFETY: Satisfies requirement to lock on output handling.
     let global_context = GLOBAL_LOG_CONTEXT.clone();
     let _unused = global_context.lock().unwrap();
 
-    // SAFETY: This call is safe if the call to rcutils_log is safe
-    //         We simply forward the parameters and apply a mutex
-    //         TODO?: Find a way to verify instead of assuming here
+    // SAFETY: This call is safe if the call to rcutils_log is safe.
+    //         We simply forward the parameters and apply a mutex.
+    //         TODO?: Find a way to verify instead of assuming here.
     rcl_logging_multiple_output_handler(location, severity, name, timestamp, format, args);
 }
 
